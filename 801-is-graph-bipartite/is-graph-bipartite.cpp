b30413f93b73de4b552c24af5ae612c00f68f907
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-06-28 20:20:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (86.58%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    bool bfs(int node, vector<vector<int>>& graph, vector<int>& col){
-        queue<int> q;
-        q.push(node);
-        col[node]=0;
-        while(!q.empty()){
-            int i=q.front();
-            q.pop();
-            for(auto it: graph[i]){
-                if(col[it]==-1){
-                    col[it]=!col[i];
-                    q.push(it);
-                }
-                else if(col[it]==col[i]) return false;
+    // bool bfs(int node, vector<vector<int>>& graph, vector<int>& col){
+    //     queue<int> q;
+    //     q.push(node);
+    //     col[node]=0;
+    //     while(!q.empty()){
+    //         int i=q.front();
+    //         q.pop();
+    //         for(auto it: graph[i]){
+    //             if(col[it]==-1){
+    //                 col[it]=!col[i];
+    //                 q.push(it);
+    //             }
+    //             else if(col[it]==col[i]) return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    bool dfs(int node, vector<vector<int>>& graph, vector<int>& col){
+        for( auto it: graph[node]){
+            if(col[it]==-1){
+                col[it]=!col[node];
+                if(!dfs(it, graph, col)) return false;
             }
+            else if(col[it]==col[node]) return false;
         }
         return true;
     }
@@ -22,7 +32,9 @@ public:
         vector<int> col(n,-1);
         for(int i=0;i<n;i++){
             if(col[i]==-1){
-                if(bfs(i, graph, col)==false){
+                col[i] = 0;
+                // if(bfs(i, graph, col)==false){
+                if(dfs(i,graph, col)==false){
                     return false;
                 }
             }
