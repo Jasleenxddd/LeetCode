13a62248df593b5999096fd8bc09956882dc36dc
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-07-12 21:15:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (57.31%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -9,16 +9,32 @@ public:
     // }
 
     // using memoization
-    int rob(int m, int n, vector<vector<int>>& dp){
-        if(m==0 && n==0) return 1;
-        if(m<0 || n<0) return 0;
-        if(dp[m][n]!=-1) return dp[m][n];
-        int left=rob(m, n-1, dp);
-        int up=rob(m-1,n, dp);
-        return dp[m][n]=left+up;
-    }
+    // int rob(int m, int n, vector<vector<int>>& dp){
+    //     if(m==0 && n==0) return 1;
+    //     if(m<0 || n<0) return 0;
+    //     if(dp[m][n]!=-1) return dp[m][n];
+    //     int left=rob(m, n-1, dp);
+    //     int up=rob(m-1,n, dp);
+    //     return dp[m][n]=left+up;
+    // }
+    // int uniquePaths(int m, int n) {
+    //     vector<vector<int>> dp(m, vector<int> (n,-1));
+    //     return rob(m-1,n-1, dp);
+    // }
+
+    // using tabulation
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n,-1));
-        return rob(m-1,n-1, dp);
+        vector<vector<int>> dp(m, vector<int> (n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]=1;
+                else{
+                    int up=(i>0)? dp[i-1][j]: 0;
+                    int left=(j>0)? dp[i][j-1]: 0;
+                    dp[i][j]=up+left;
+                } 
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
