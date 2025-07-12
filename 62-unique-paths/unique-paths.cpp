e05c10acf7f76bacf4021baaaa7ec708b35d6a2c
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-07-13 00:54:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (80.13%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -23,18 +23,31 @@ public:
     // }
 
     // using tabulation
+    // int uniquePaths(int m, int n) {
+    //     vector<vector<int>> dp(m, vector<int> (n,0));
+    //     for(int i=0;i<m;i++){
+    //         for(int j=0;j<n;j++){
+    //             if(i==0 && j==0) dp[i][j]=1;
+    //             else{
+    //                 int up=(i>0)? dp[i-1][j]: 0;
+    //                 int left=(j>0)? dp[i][j-1]: 0;
+    //                 dp[i][j]=up+left;
+    //             } 
+    //         }
+    //     }
+    //     return dp[m-1][n-1];
+    // }
+
+    // using space optimization
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n,0));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0) dp[i][j]=1;
-                else{
-                    int up=(i>0)? dp[i-1][j]: 0;
-                    int left=(j>0)? dp[i][j-1]: 0;
-                    dp[i][j]=up+left;
-                } 
+        vector<int> dp(n, 1);
+        for(int i=1;i<m;i++){
+            vector<int> tmp(n, 1);
+            for(int j=1;j<n;j++){
+                tmp[j]=dp[j]+tmp[j-1];
             }
+            dp=tmp;
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 };
