ef75e0731c23e32838f3e0cc23891d46ee1d8cc2
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-07-15 23:55:58 +0530

    Time: 3 ms (15.74%) | Memory: 13.4 MB (11.60%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int tri(int i, int j, vector<vector<int>>& triangle, int n, vector<vector<int>>& dp){
+        if(i==n-1) return triangle[n-1][j];
+        if(dp[i][j]!=-1) return dp[i][j];
+        int down= triangle[i][j] + tri(i+1, j, triangle, n, dp);
+        int diag= triangle[i][j] + tri(i+1, j+1, triangle, n, dp);
+        return dp[i][j]= min(down, diag);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        vector<vector<int>> dp(n, vector<int> (n, -1));
+        return tri(0,0, triangle, n, dp);
+    }
+};
