7cbf9116e99faed727ab968a694756944cfc08f3
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-07-16 00:32:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (81.64%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -27,19 +27,38 @@ public:
     // }
 
     // using tabulation
+    // int minimumTotal(vector<vector<int>>& triangle) {
+    //     int n=triangle.size();
+    //     vector<vector<int>> dp(n, vector<int> (n, -1));
+    //     for(int j=0;j<n;j++){
+    //         dp[n-1][j]=triangle[n-1][j];
+    //     }
+    //     for(int i=n-2;i>=0;i--){
+    //         for(int j=0;j<=i;j++){
+    //             int down= triangle[i][j] + dp[i+1][j];
+    //             int diag= triangle[i][j] + dp[i+1][j+1];
+    //             dp[i][j]=min(down, diag);
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
+
+    // using space optimization
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>> dp(n, vector<int> (n, -1));
+        vector<int> last(n, 0);
+        vector<int> curr(n, 0);
         for(int j=0;j<n;j++){
-            dp[n-1][j]=triangle[n-1][j];
+            last[j]=triangle[n-1][j];
         }
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<=i;j++){
-                int down= triangle[i][j] + dp[i+1][j];
-                int diag= triangle[i][j] + dp[i+1][j+1];
-                dp[i][j]=min(down, diag);
+                int down= triangle[i][j] + last[j];
+                int diag= triangle[i][j] + last[j+1];
+                curr[j]=min(down, diag);
             }
+            last=curr;
         }
-        return dp[0][0];
+        return last[0];
     }
 };
