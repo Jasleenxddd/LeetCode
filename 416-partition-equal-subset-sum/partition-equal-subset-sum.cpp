e211ed7ba693cc0fa4e321734bbd27bb9b307a77
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-07-21 18:22:46 +0530

    Time: 79 ms (83.42%) | Memory: 15.6 MB (76.87%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -23,6 +23,31 @@ public:
     // }
 
     // using tabulation
+    // bool canPartition(vector<int>& nums) {
+    //     int n=nums.size();
+    //     int sum=0;
+    //     for(int i=0;i<n;i++){
+    //         sum+=nums[i];
+    //     }
+    //     if(sum%2!=0) return false;
+    //     int tar=sum/2;
+    //     vector<vector<int>> dp(n, vector<int> (tar+1, 0));
+    //     for(int i=0;i<n;i++){
+    //         dp[i][0]=true;
+    //     }
+    //     if(nums[0]<=tar) dp[0][nums[0]]=true;
+    //     for(int i=1;i<n;i++){
+    //         for(int j=1;j<=tar;j++){
+    //             bool notTake= dp[i-1][j];
+    //             bool take=false;
+    //             if(j>= nums[i]) take=dp[i-1][j-nums[i]];
+    //             dp[i][j]= take || notTake;
+    //         }
+    //     }
+    //     return dp[n-1][tar];
+    // }
+
+    // using space optimization
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
@@ -31,19 +56,20 @@ public:
         }
         if(sum%2!=0) return false;
         int tar=sum/2;
-        vector<vector<int>> dp(n, vector<int> (tar+1, 0));
-        for(int i=0;i<n;i++){
-            dp[i][0]=true;
-        }
-        if(nums[0]<=tar) dp[0][nums[0]]=true;
+        vector<int> prev(tar+1, 0);
+        vector<int> curr(tar+1, 0);
+        prev[0]=true;
+        curr[0]=true;
+        if(nums[0]<=tar) prev[nums[0]]=true;
         for(int i=1;i<n;i++){
             for(int j=1;j<=tar;j++){
-                bool notTake= dp[i-1][j];
+                bool notTake= prev[j];
                 bool take=false;
-                if(j>= nums[i]) take=dp[i-1][j-nums[i]];
-                dp[i][j]= take || notTake;
+                if(j>= nums[i]) take=prev[j-nums[i]];
+                curr[j]= take || notTake;
             }
+            prev=curr;
         }
-        return dp[n-1][tar];
+        return prev[tar];
     }
 };
