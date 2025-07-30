3665e1460242e48d92dd01b1a77408d895adc1fa
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-07-31 02:49:09 +0530

    Time: 377 ms (66.92%) | Memory: 73.5 MB (31.33%) - LeetSync

---
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums) {
+        int N=nums.size();
+        int res=INT_MAX;
+        int sum=0;
+        sum=accumulate(nums.begin(), nums.end(), 0);
+        
+        int n=N/2;
+        vector<vector<int>> left(n+1), right(n+1);
+
+        for(int mask=0; mask<(1<<n); mask++){
+            int sz=0, l=0, r=0;
+            for(int i=0;i<n;i++){
+                if(mask&(1<<i)){
+                    sz++;
+                    l+=nums[i];
+                    r+=nums[n+i];
+                }
+            }
+            left[sz].push_back(l);
+            right[sz].push_back(r);
+        }
+        
+        for(int i=0;i<=n;i++){
+            sort(right[i].begin(), right[i].end());
+        }
+
+        for(int sz=0;sz<n;sz++){
+            for(auto& a: left[sz]){
+                int b=(sum-2*a)/2, rsz=n-sz;
+                auto& v=right[rsz];
+                auto itr=lower_bound(v.begin(), v.end(), b);
+                if(itr!=v.end()) res=min(res, abs(sum-2*(a+(*itr))));
+                if(itr!=v.begin()){
+                    auto it=itr;
+                    it--;
+                    res=min(res, abs(sum-2*(a+(*it))));
+                }
+            }
+        }
+        return res;
+    }
+};
