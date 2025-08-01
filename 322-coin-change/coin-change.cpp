5fb4655609592d8b3854ac4a0424242d433fd1b9
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-08-02 01:03:26 +0530

    Time: 38 ms (53.46%) | Memory: 22.1 MB (52.72%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -21,22 +21,44 @@ public:
     // }
 
     // using tabulation
+    // int coinChange(vector<int>& coins, int amount) {
+    //     int n=coins.size();
+    //     vector<vector<int>> dp(n, vector<int> (amount+1, 0));
+    //     for(int t=0;t<=amount; t++){
+    //         if(t%coins[0]==0) dp[0][t]= t/ coins[0];
+    //         else dp[0][t]= 1e9;
+    //     }
+    //     for(int i=1; i<n; i++){
+    //         for(int t=0; t<=amount; t++){
+    //             int notPick=dp[i-1][t]; 
+    //             int pick=1e9;
+    //             if(coins[i]<=t) pick=1+dp[i][t-coins[i]];
+    //             dp[i][t]= min(pick, notPick);
+    //         }
+    //     }
+    //     int ans= dp[n-1][amount];
+    //     if(ans>=1e9) return -1;
+    //     else return ans;
+    // }
+
+    // using space optimization
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1, 0));
+        vector<int> prev(amount+1, 0), curr(amount+1, 0);
         for(int t=0;t<=amount; t++){
-            if(t%coins[0]==0) dp[0][t]= t/ coins[0];
-            else dp[0][t]= 1e9;
+            if(t%coins[0]==0) prev[t]= t/ coins[0];
+            else prev[t]= 1e9;
         }
         for(int i=1; i<n; i++){
             for(int t=0; t<=amount; t++){
-                int notPick=dp[i-1][t]; 
+                int notPick=prev[t]; 
                 int pick=1e9;
-                if(coins[i]<=t) pick=1+dp[i][t-coins[i]];
-                dp[i][t]= min(pick, notPick);
+                if(coins[i]<=t) pick=1+curr[t-coins[i]];
+                curr[t]= min(pick, notPick);
             }
+            prev=curr;
         }
-        int ans= dp[n-1][amount];
+        int ans= prev[amount];
         if(ans>=1e9) return -1;
         else return ans;
     }
