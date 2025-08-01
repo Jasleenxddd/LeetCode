dcbcab89b226762e572d1cc7653c4dc71a9aac39
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-08-02 00:56:34 +0530

    Time: 46 ms (39.42%) | Memory: 48.8 MB (48.12%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    // using memoization 
+    // int f(int i, vector<int>& coins, int amount, vector<vector<int>>& dp){
+    //     if(i==0){
+    //         if(amount%coins[0]==0) return amount/coins[0];
+    //         else return 1e9;
+    //     }
+    //     if(dp[i][amount]!=-1) return dp[i][amount];
+    //     int notPick=f(i-1, coins, amount, dp);
+    //     int pick=1e9;
+    //     if(coins[i]<=amount) pick=1+f(i, coins, amount-coins[i], dp);
+    //     return min(pick, notPick);
+    // }
+    // int coinChange(vector<int>& coins, int amount) {
+    //     int n=coins.size();
+    //     vector<vector<int>> dp(n, vector<int> (amount+1, -1));
+    //     int ans= f(n-1, coins, amount, dp);
+    //     if(ans>=1e9) return -1;
+    //     else return ans;
+    // }
+
+    // using tabulation
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>> dp(n, vector<int> (amount+1, 0));
+        for(int t=0;t<=amount; t++){
+            if(t%coins[0]==0) dp[0][t]= t/ coins[0];
+            else dp[0][t]= 1e9;
+        }
+        for(int i=1; i<n; i++){
+            for(int t=0; t<=amount; t++){
+                int notPick=dp[i-1][t]; 
+                int pick=1e9;
+                if(coins[i]<=t) pick=1+dp[i][t-coins[i]];
+                dp[i][t]= min(pick, notPick);
+            }
+        }
+        int ans= dp[n-1][amount];
+        if(ans>=1e9) return -1;
+        else return ans;
+    }
+};
