1767b1b722ee28f2cc1ff9a48656717bd0b06eac
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-08-02 15:00:24 +0530

    Time: 3 ms (87.66%) | Memory: 15.3 MB (52.10%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -18,24 +18,47 @@ public:
     // }
 
     // using memoization or recursion
-    int cntDiff(int i, int target, vector<int>& arr, vector<vector<int>>& dp){
-        if(i==0){
-            if(target==0 && arr[0]==0) return 2;
-            if(target==0 || target==arr[0]) return 1;
-            return 0;
-        }
-        if(dp[i][target]!=-1) return dp[i][target];
-        int notPick=cntDiff(i-1, target, arr, dp);
-        int pick=0;
-        if(arr[i]<=target) pick=cntDiff(i-1, target-arr[i], arr, dp);
-        return dp[i][target]=pick+notPick;
-    }
+    // int cntDiff(int i, int target, vector<int>& arr, vector<vector<int>>& dp){
+    //     if(i==0){
+    //         if(target==0 && arr[0]==0) return 2;
+    //         if(target==0 || target==arr[0]) return 1;
+    //         return 0;
+    //     }
+    //     if(dp[i][target]!=-1) return dp[i][target];
+    //     int notPick=cntDiff(i-1, target, arr, dp);
+    //     int pick=0;
+    //     if(arr[i]<=target) pick=cntDiff(i-1, target-arr[i], arr, dp);
+    //     return dp[i][target]=pick+notPick;
+    // }
+    // int findTargetSumWays(vector<int>& nums, int d) {
+    //     int n=nums.size();
+    //     int sum=accumulate(nums.begin(), nums.end(), 0);
+    //     int target=(sum-d)/2;
+    //     if(sum-d<0 || (sum-d)%2!=0) return 0;
+    //     vector<vector<int>> dp(n, vector<int> (target+1, -1));
+    //     return cntDiff(n-1, target, nums, dp);
+    // }
+
+    // using tabulation
     int findTargetSumWays(vector<int>& nums, int d) {
         int n=nums.size();
         int sum=accumulate(nums.begin(), nums.end(), 0);
         int target=(sum-d)/2;
         if(sum-d<0 || (sum-d)%2!=0) return 0;
-        vector<vector<int>> dp(n, vector<int> (target+1, -1));
-        return cntDiff(n-1, target, nums, dp);
+        vector<vector<int>> dp(n, vector<int> (target+1, 0));
+        if(nums[0]==0) dp[0][0]=2;
+        else{
+            dp[0][0]=1;
+            if( nums[0]<=target) dp[0][nums[0]]=1;
+        }
+        for(int i=1; i<n;i++){
+            for(int sum=0; sum<=target;sum++){
+                int notPick=dp[i-1][sum];
+                int pick=0;
+                if(nums[i]<=sum) pick=dp[i-1][sum-nums[i]];
+                dp[i][sum]= pick+notPick;
+            }
+        }
+        return dp[n-1][target];
     }
 };
