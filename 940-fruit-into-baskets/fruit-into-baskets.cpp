810aca244163a645597d779a960e2176f93144a6
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-08-06 02:17:17 +0530

    Time: 32 ms (76.66%) | Memory: 75.2 MB (28.03%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int,int> map;
-        int l=0,r=0,len=0;
-        for(r=0;r<fruits.size();r++){
-            map[fruits[r]]++;
-            if(map.size()>2){
-                map[fruits[l]]--;
-                if(map[fruits[l]]==0){
-                    map.erase(fruits[l]);
+        unordered_map<int, int> mpp;
+        int l=0, len=0;
+        for(int r=0; r<fruits.size();r++){
+            mpp[fruits[r]]++;
+            if(mpp.size()>2){
+                mpp[fruits[l]]--;
+                if(mpp[fruits[l]]==0){
+                    mpp.erase(fruits[l]);
                 }
                 l++;
             }
-            if(map.size()<=2){
-                len=max(len,r-l+1);
-            }
+            if(mpp.size()<=2) len=max(len, r-l+1);
         }
         return len;
     }
