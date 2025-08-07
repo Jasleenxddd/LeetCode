7c68aac4f0b75d0a44ea76f589847ee697eee2ae
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-08-08 00:23:21 +0530

    Time: 63 ms (10.65%) | Memory: 31.5 MB (58.31%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
-    // using tabulation
-    int lcs(string s, string t){
-        int n=s.size();
-        vector<vector<int>> dp(n+1, vector<int> (n+1,0));
-        for(int i=0; i<=n; i++) dp[i][0]=0;
-        for(int j=0; j<=n; j++) dp[0][j]=0;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                if(s[i-1]==t[j-1]) dp[i][j]=1+ dp[i-1][j-1];
-                else dp[i][j]= max(dp[i-1][j], dp[i][j-1]);
-            }
-        }
-        return dp[n][n];
+    // using memoization 
+    int f(int i, int j, string& text1, string& text2, vector<vector<int>>& dp){
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(text1[i]==text2[j]) return dp[i][j]=1+f(i-1, j-1, text1, text2, dp);
+        return dp[i][j]=max(f(i-1, j, text1, text2, dp), f(i,j-1, text1, text2, dp));
+    }
+    int lcs(string text1, string text2) {
+        int n=text1.length();
+        int m=text2.length();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return f(n-1, m-1, text1, text2, dp);
     }
     int longestPalindromeSubseq(string s) {
         string t=s;
         reverse(t.begin(), t.end());
         return lcs(s, t);
     }
-    
+
+    // using tabulation
+    // int lcs(string s, string t){
+    //     int n=s.size();
+    //     vector<vector<int>> dp(n+1, vector<int> (n+1,0));
+    //     for(int i=0; i<=n; i++) dp[i][0]=0;
+    //     for(int j=0; j<=n; j++) dp[0][j]=0;
+    //     for(int i=1; i<=n; i++){
+    //         for(int j=1; j<=n; j++){
+    //             if(s[i-1]==t[j-1]) dp[i][j]=1+ dp[i-1][j-1];
+    //             else dp[i][j]= max(dp[i-1][j], dp[i][j-1]);
+    //         }
+    //     }
+    //     return dp[n][n];
+    // }
+    // int longestPalindromeSubseq(string s) {
+    //     string t=s;
+    //     reverse(t.begin(), t.end());
+    //     return lcs(s, t);
+    // }
+
     // using space optimization
     // int lcs(string s, string t){
     //     int n=s.size();
