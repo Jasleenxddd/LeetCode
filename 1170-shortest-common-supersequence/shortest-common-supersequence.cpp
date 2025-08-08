68c75063830f38cbec7227fcabd299441a781380
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-08-08 19:50:00 +0530

    Time: 31 ms (9.63%) | Memory: 27.1 MB (12.34%) - LeetSync

---
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    // using memoization 
+    int f(int i, int j, string& text1, string& text2, vector<vector<int>>& dp){
+        if(i==0 || j==0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(text1[i-1]==text2[j-1]) return dp[i][j]=1+f(i-1, j-1, text1, text2, dp);
+        return dp[i][j]=max(f(i-1, j, text1, text2, dp), f(i,j-1, text1, text2, dp));
+    }
+    string scs(string& text1, string& text2, vector<vector<int>>& dp ){
+        int i=text1.length();
+        int j=text2.length();
+        string ans="";
+        while(i>0 && j>0){
+            if(text1[i-1]== text2[j-1]){
+                ans+=text1[i-1];
+                i--;
+                j--;
+            }
+            else if(dp[i-1][j]> dp[i][j-1]){
+                ans+=text1[i-1];
+                i--;
+            }
+            else{
+                ans+=text2[j-1];
+                j--;
+            }
+        } 
+        while(i>0){
+            ans+=text1[i-1];
+            i--;
+        }
+        while(j>0){
+            ans+=text2[j-1];
+            j--;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+    string shortestCommonSupersequence(string text1, string text2) {
+        int n=text1.length();
+        int m=text2.length();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        f(n, m, text1, text2, dp);
+        return scs(text1, text2, dp);
+    }
+};
