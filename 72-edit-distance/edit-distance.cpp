8f1434e171125d78ec232757e4f62f7d50d0dc63
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-08-11 20:07:40 +0530

    Time: 8 ms (45.08%) | Memory: 14.5 MB (16.04%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
+    // using memoization
+    // int f(int i, int j, string& word1, string& word2, vector<vector<int>>& dp){
+    //     if(i<0) return j+1;
+    //     if(j<0) return i+1;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(word1[i]==word2[j]) return dp[i][j]= 0+f(i-1, j-1, word1, word2, dp);
+    //     return dp[i][j]= 1+min({f(i-1, j, word1, word2, dp), f(i, j-1, word1, word2, dp), f(i-1, j-1, word1, word2, dp)});
+    // }
+    // int minDistance(string word1, string word2) {
+    //     int n=word1.length();
+    //     int m=word2.length();
+    //     vector<vector<int>> dp(n, vector<int> (m+1, -1));
+    //     return f(n-1, m-1, word1, word2, dp);
+    // }
+
+    // shifting of index
     int f(int i, int j, string& word1, string& word2, vector<vector<int>>& dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
+        if(i==0) return j;
+        if(j==0) return i;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(word1[i]==word2[j]) return dp[i][j]= 0+f(i-1, j-1, word1, word2, dp);
+        if(word1[i-1]==word2[j-1]) return dp[i][j]= 0+f(i-1, j-1, word1, word2, dp);
         return dp[i][j]= 1+min({f(i-1, j, word1, word2, dp), f(i, j-1, word1, word2, dp), f(i-1, j-1, word1, word2, dp)});
     }
     int minDistance(string word1, string word2) {
         int n=word1.length();
         int m=word2.length();
-        vector<vector<int>> dp(n, vector<int> (m+1, -1));
-        return f(n-1, m-1, word1, word2, dp);
+        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
+        return f(n, m, word1, word2, dp);
     }
 };
