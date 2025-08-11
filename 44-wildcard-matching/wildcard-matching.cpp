391acb6d326e3f489d0c092b51ee83322abde604
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-08-12 00:35:09 +0530

    Time: 38 ms (26.00%) | Memory: 31.7 MB (22.98%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,23 +1,46 @@
 class Solution {
 public:
+    // using memoization
+    // bool f(int i, int j, string& s, string& p, vector<vector<int>>& dp){
+    //     if(i<0 && j<0 ) return true;
+    //     if(j<0 && i>=0 ) return false;
+    //     if(i<0 && j>=0){
+    //         for(int k=0; k<=j; k++){
+    //             if(p[k]!='*') return false;
+    //         }
+    //         return true;
+    //     }
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(s[i]== p[j] || p[j]=='?') return dp[i][j]=f(i-1, j-1, s, p, dp);
+    //     if(p[j]=='*') return dp[i][j]=f(i-1, j, s, p, dp) || f(i, j-1, s, p, dp);
+    //     return false;
+    // }
+    // bool isMatch(string s, string p) {
+    //     int n=s.length();
+    //     int m=p.length();
+    //     vector<vector<int>> dp(n, vector<int> (m, -1));
+    //     return f(n-1, m-1, s, p, dp);
+    // }
+
+    // shifting of index
     bool f(int i, int j, string& s, string& p, vector<vector<int>>& dp){
-        if(i<0 && j<0 ) return true;
-        if(j<0 && i>=0 ) return false;
-        if(i<0 && j>=0){
-            for(int k=0; k<=j; k++){
-                if(p[k]!='*') return false;
+        if(i==0 && j==0 ) return true;
+        if(j==0 && i>0 ) return false;
+        if(i==0 && j>0){
+            for(int k=1; k<=j; k++){
+                if(p[k-1]!='*') return false;
             }
             return true;
         }
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]== p[j] || p[j]=='?') return dp[i][j]=f(i-1, j-1, s, p, dp);
-        if(p[j]=='*') return dp[i][j]=f(i-1, j, s, p, dp) || f(i, j-1, s, p, dp);
+        if(s[i-1]== p[j-1] || p[j-1]=='?') return dp[i][j]=f(i-1, j-1, s, p, dp);
+        if(p[j-1]=='*') return dp[i][j]=f(i-1, j, s, p, dp) || f(i, j-1, s, p, dp);
         return false;
     }
     bool isMatch(string s, string p) {
         int n=s.length();
         int m=p.length();
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        return f(n-1, m-1, s, p, dp);
+        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
+        return f(n, m, s, p, dp);
     }
 };
