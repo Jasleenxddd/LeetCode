08b0c25f444d886fc67759579d51d5fe504b5b12
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-08-11 20:29:32 +0530

    Time: 5 ms (71.64%) | Memory: 10.1 MB (99.27%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -16,17 +16,50 @@ public:
     // }
 
     // shifting of index
-    int f(int i, int j, string& word1, string& word2, vector<vector<int>>& dp){
-        if(i==0) return j;
-        if(j==0) return i;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(word1[i-1]==word2[j-1]) return dp[i][j]= 0+f(i-1, j-1, word1, word2, dp);
-        return dp[i][j]= 1+min({f(i-1, j, word1, word2, dp), f(i, j-1, word1, word2, dp), f(i-1, j-1, word1, word2, dp)});
-    }
+    // int f(int i, int j, string& word1, string& word2, vector<vector<int>>& dp){
+    //     if(i==0) return j;
+    //     if(j==0) return i;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(word1[i-1]==word2[j-1]) return dp[i][j]= 0+f(i-1, j-1, word1, word2, dp);
+    //     return dp[i][j]= 1+min({f(i-1, j, word1, word2, dp), f(i, j-1, word1, word2, dp), f(i-1, j-1, word1, word2, dp)});
+    // }
+    // int minDistance(string word1, string word2) {
+    //     int n=word1.length();
+    //     int m=word2.length();
+    //     vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
+    //     return f(n, m, word1, word2, dp);
+    // }
+
+    // using tabulation
+    // int minDistance(string word1, string word2) {
+    //     int n=word1.length();
+    //     int m=word2.length();
+    //     vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+    //     for(int i=0; i<=n; i++) dp[i][0]=i;
+    //     for(int j=0; j<=m; j++) dp[0][j]=j;
+    //     for(int i=1; i<=n; i++){
+    //         for(int j=1; j<=m; j++){
+    //             if(word1[i-1]==word2[j-1]) dp[i][j]= 0+ dp[i-1][j-1];
+    //             else dp[i][j]= 1+min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+    //         }
+    //     }
+    //     return dp[n][m];
+    // }
+
+    // using space optimization
     int minDistance(string word1, string word2) {
         int n=word1.length();
         int m=word2.length();
-        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
-        return f(n, m, word1, word2, dp);
+        vector<int> curr(m+1, 0), prev(m+1, 0);
+        for(int j=0; j<=m; j++) prev[j]=j;
+        for(int i=1; i<=n; i++){
+            curr[0]=i;
+            for(int j=1; j<=m; j++){
+                if(word1[i-1]==word2[j-1]) curr[j]= 0+ prev[j-1];
+                else curr[j]= 1+min({prev[j], curr[j-1], prev[j-1]});
+            }
+            prev=curr;
+        }
+        return prev[m];
     }
 };
