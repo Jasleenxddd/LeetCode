8bba4eed90c28a318d56d13bac81272aa5d87a98
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-08-12 21:59:29 +0530

    Time: 60 ms (69.36%) | Memory: 79.2 MB (97.18%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -14,31 +14,48 @@ public:
     // }
 
     // using tabulation 
+    // int maxProfit(vector<int>& prices) {
+    //     int n=prices.size();
+    //     vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (3, 0)));
+    //     // no need for base case as it is already initialized to 0 
+
+    //     // in case
+    //     // for(int i=0; i<n; i ++){
+    //     //     for(int buy=0; buy<=1; buy++){
+    //     //         dp[i][buy][0]=0;
+    //     //     }
+    //     // }
+    //     // for(int buy=0; buy<=1; buy++){
+    //     //     for(int cap=0; cap<=2; cap++){
+    //     //         dp[0][buy][cap]=0;
+    //     //     }
+    //     // }
+
+    //     for(int i=n-1; i>=0; i--){
+    //         for(int buy=0; buy<=1; buy++){
+    //             for(int cap=1; cap<=2; cap++){
+    //                 if(buy) dp[i][buy][cap]=max(-prices[i]+dp[i+1][0][cap], dp[i+1][1][cap]);
+    //                 else dp[i][buy][cap]=max(prices[i]+ dp[i+1][1][cap-1], dp[i+1][0][cap]);
+    //             }
+    //         }
+    //     }
+    //     return dp[0][1][2];
+    // }
+
+    // using space optimization
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (3, 0)));
-        // no need for base case as it is already initialized to 0 
-
-        // in case
-        // for(int i=0; i<n; i ++){
-        //     for(int buy=0; buy<=1; buy++){
-        //         dp[i][buy][0]=0;
-        //     }
-        // }
-        // for(int buy=0; buy<=1; buy++){
-        //     for(int cap=0; cap<=2; cap++){
-        //         dp[0][buy][cap]=0;
-        //     }
-        // }
-
+        vector<vector<int>> ahead(2, vector<int> (3, 0));
+        vector<vector<int>> curr(2, vector<int> (3, 0));
         for(int i=n-1; i>=0; i--){
             for(int buy=0; buy<=1; buy++){
                 for(int cap=1; cap<=2; cap++){
-                    if(buy) dp[i][buy][cap]=max(-prices[i]+dp[i+1][0][cap], dp[i+1][1][cap]);
-                    else dp[i][buy][cap]=max(prices[i]+ dp[i+1][1][cap-1], dp[i+1][0][cap]);
+                    if(buy) curr[buy][cap]=max(-prices[i]+ahead[0][cap], ahead[1][cap]);
+                    else curr[buy][cap]=max(prices[i]+ ahead[1][cap-1], ahead[0][cap]);
                 }
             }
+            ahead=curr;
         }
-        return dp[0][1][2];
+        return ahead[1][2];
     }
 };
