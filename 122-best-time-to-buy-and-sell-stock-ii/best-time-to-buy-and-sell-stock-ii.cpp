1c47bd33d45a8a240e76ea4fb5a6241d6dafb4c7
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-08-12 19:25:19 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (98.11%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -30,17 +30,31 @@ public:
     // }  
 
     // using space optimization
+    // int maxProfit(vector<int>& prices) {
+    //     int n=prices.size();
+    //     vector<int> ahead(2, 0), curr(2, 0);
+    //     ahead[0]=ahead[1]=0;
+    //     for(int i=n-1; i>=0; i--){
+    //         for(int buy=0; buy<=1; buy++){
+    //             if(buy) curr[buy]=max(-prices[i]+ ahead[0], 0+ ahead[1]);
+    //             else curr[buy]=max(prices[i]+ ahead[1], ahead[0]);
+    //         }
+    //         ahead=curr;
+    //     }
+    //     return ahead[1];
+    // }  
+
+    // using 4 variables
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<int> ahead(2, 0), curr(2, 0);
-        ahead[0]=ahead[1]=0;
+        int aheadNotBuy, aheadBuy, currNotBuy, currBuy;
+        aheadNotBuy=aheadBuy=0;
         for(int i=n-1; i>=0; i--){
-            for(int buy=0; buy<=1; buy++){
-                if(buy) curr[buy]=max(-prices[i]+ ahead[0], 0+ ahead[1]);
-                else curr[buy]=max(prices[i]+ ahead[1], ahead[0]);
-            }
-            ahead=curr;
+            currBuy=max(-prices[i]+ aheadNotBuy, 0+ aheadBuy);
+            currNotBuy=max(prices[i]+ aheadBuy, aheadNotBuy);
+            aheadNotBuy=currNotBuy;
+            aheadBuy=currBuy;
         }
-        return ahead[1];
+        return aheadBuy;
     }  
 };
