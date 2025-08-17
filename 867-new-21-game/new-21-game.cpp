f541e9634861103ad589712da132441f60ab1385
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-08-18 01:58:58 +0530

    Time: 2 ms (75.96%) | Memory: 12 MB (97.84%) - LeetSync

---
+++ 867-new-21-game/new-21-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    double new21Game(int n, int k, int maxPts) {
+        if(k== 0 ||n >= k-1+maxPts) return 1.0;
+        vector<double> dp(maxPts, 0.0);
+        dp[0]= 1.0;
+        double sum= 1.0, res= 0.0;
+        for(int i= 1; i<= n; i++){
+            double prob= sum/ maxPts;
+            if(i< k){
+                sum+= prob;
+            }else{
+                res+= prob;
+            }
+            if(i>= maxPts){
+                sum-= dp[i%maxPts];
+            }
+            dp[i% maxPts]= prob;
+        }
+        return res;
+    }
+};
