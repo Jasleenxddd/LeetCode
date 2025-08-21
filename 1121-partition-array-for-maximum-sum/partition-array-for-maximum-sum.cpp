a86e8c181821a71f9165bb50163323bc6c15cfa4
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-08-21 17:39:44 +0530

    Time: 2 ms (89.33%) | Memory: 12.4 MB (87.15%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
-    int f(int i, vector<int>& arr, int k, vector<int>& dp){
-        if(i== arr.size()) return 0;
-        int len=0;
-        int maxi=INT_MIN;
-        int ans=INT_MIN;
-        if(dp[i]!=-1) return dp[i];
-        for(int j=i; j<min(i+k, (int)arr.size()); j++){
-            len++;
-            maxi=max(maxi, arr[j]);
-            int sum= len*maxi + f(j+1, arr, k, dp);
-            ans=max(ans, sum);
-        }
-        return dp[i]=ans;
-    }
+    // using memoization
+    // int f(int i, vector<int>& arr, int k, vector<int>& dp){
+    //     if(i== arr.size()) return 0;
+    //     int len=0;
+    //     int maxi=INT_MIN;
+    //     int ans=INT_MIN;
+    //     if(dp[i]!=-1) return dp[i];
+    //     for(int j=i; j<min(i+k, (int)arr.size()); j++){
+    //         len++;
+    //         maxi=max(maxi, arr[j]);
+    //         int sum= len*maxi + f(j+1, arr, k, dp);
+    //         ans=max(ans, sum);
+    //     }
+    //     return dp[i]=ans;
+    // }
+    // int maxSumAfterPartitioning(vector<int>& arr, int k) {
+    //     int n=arr.size();
+    //     vector<int> dp(n, -1);
+    //     return f(0, arr, k, dp);
+    // }
+
+    // using tabulation
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
-        vector<int> dp(n, -1);
-        return f(0, arr, k, dp);
+        vector<int> dp(n+1, 0);
+        for(int i=n-1; i>=0; i--){
+            int len=0;
+            int maxi=INT_MIN;
+            int ans=0;
+            for(int j=i; j<min(i+k, (int)arr.size()); j++){
+                len++;
+                maxi=max(maxi, arr[j]);
+                int sum= len*maxi + dp[j+1];
+                ans=max(ans, sum);
+            }
+            dp[i]=ans;
+        }
+        return dp[0];
     }
 };
