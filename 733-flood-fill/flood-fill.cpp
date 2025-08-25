ac9d0176520188d4fc50da3563fe1d2650cc255e
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-08-25 21:02:26 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (47.58%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        // int n=image.size();
+        // int m=image[0].size();
+        // queue<pair<int,int>> q;
+        // int prev=image[sr][sc];
+        // if(prev==color) return image;
+        // q.push({sr,sc});
+        // image[sr][sc]=color;
+        // vector<pair<int,int>> dir={{-1,0},{0,1},{0,-1},{1,0}};
+
+        // while(!q.empty()){
+        //     auto [r,c]= q.front();
+        //     q.pop();
+        //     for(auto [dr,dc]:dir){
+        //         int nr=r+dr;
+        //         int nc=c+dc;
+        //         if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc]==prev){
+        //             image[nr][nc]=color;
+        //             q.push({nr,nc});
+        //         }
+        //     }
+            
+        // }
+        // return image;
+
         int n=image.size();
         int m=image[0].size();
-        queue<pair<int,int>> q;
+        queue<pair<int, int>> q;
         int prev=image[sr][sc];
         if(prev==color) return image;
-        q.push({sr,sc});
+        q.push({sr, sc});
         image[sr][sc]=color;
-        vector<pair<int,int>> dir={{-1,0},{0,1},{0,-1},{1,0}};
-
+        vector<pair<int, int>> dir={{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
         while(!q.empty()){
-            auto [r,c]= q.front();
+            auto [r, c]=q.front();
             q.pop();
-            for(auto [dr,dc]:dir){
+            for(auto [dr, dc]: dir){
                 int nr=r+dr;
                 int nc=c+dc;
                 if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc]==prev){
                     image[nr][nc]=color;
-                    q.push({nr,nc});
+                    q.push({nr, nc});
                 }
             }
-            
         }
         return image;
     }
