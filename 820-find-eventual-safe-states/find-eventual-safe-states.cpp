f13aec8c4d55269a718e1225d1d324a00afe481b
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-09-09 20:28:08 +0530

    Time: 51 ms (43.21%) | Memory: 66.4 MB (21.65%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,30 +1,53 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& graph, vector<int>& vis, vector<int>& path, vector<int>& safe){
-        vis[node]=1;
-        path[node]=1;
-        for(auto it: graph[node]){
-            if(!vis[it]){
-                if(dfs(it, graph, vis, path, safe)==true) return true;
-            }
-            else if(path[it]) return true;
-        }
-        safe[node]=1;
-        path[node]=0;
-        return false;
-    }
+    // bool dfs(int node, vector<vector<int>>& graph, vector<int>& vis, vector<int>& path, vector<int>& safe){
+    //     vis[node]=1;
+    //     path[node]=1;
+    //     for(auto it: graph[node]){  
+    //         if(!vis[it]){
+    //             if(dfs(it, graph, vis, path, safe)==true) return true;
+    //         }
+    //         else if(path[it]) return true;
+    //     }
+    //     safe[node]=1;
+    //     path[node]=0;
+    //     return false;
+    // }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<int> vis(n);
-        vector<int> path(n);
-        vector<int> safe(n);
-        vector<int> safeStates;
-        for(int i=0;i<n;i++){
-            if(!vis[i]) dfs(i, graph, vis, path, safe);
+        // vector<int> vis(n);
+        // vector<int> path(n);
+        // vector<int> safe(n);
+        // vector<int> safeStates;
+        // for(int i=0;i<n;i++){
+        //     if(!vis[i]) dfs(i, graph, vis, path, safe);
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(safe[i]==1) safeStates.push_back(i);
+        // }
+        vector<vector<int>> adj(n);
+        vector<int> outdegree(n);
+        for(int i=0; i<n; i++){
+            for(auto it: graph[i]){
+                adj[it].push_back(i);
+                outdegree[i]++;
+            }
         }
-        for(int i=0;i<n;i++){
-            if(safe[i]==1) safeStates.push_back(i);
+        queue<int> q;
+        for(int i=0; i<n; i++){
+            if(outdegree[i]==0) q.push(i);
         }
-        return safeStates;
+        vector<int> safe;
+        while(!q.empty()){
+            int i=q.front();
+            q.pop();
+            safe.push_back(i);
+            for(auto it: adj[i]){
+                outdegree[it]--;
+                if(outdegree[it]==0) q.push(it);
+            }
+        }
+        sort(safe.begin(), safe.end());
+        return safe;
     }
 };
