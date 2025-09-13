e4f7c12f2cd4df81413bff34ee3121b2ba803ae5
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-09-13 15:28:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (22.40%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
-        int v=0, c=0;
         int maxiV=0, maxiC=0;
-        unordered_map<char, int> mpp;
+        unordered_map<char, int> v, c;
         for(int i=0; i<s.length(); i++){
-            mpp[s[i]]++;
-        }  
-        for(auto& it: mpp){
-            if(it.first=='a' || it.first=='e' || it.first=='i' || it.first=='o' || it.first=='u')
-                maxiV=max(maxiV, it.second);
-            else   
-                maxiC=max(maxiC, it.second);
+            char ch=s[i];
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                v[ch]++;
+                maxiV=max(maxiV, v[ch]);
+            }
+            else{
+                c[ch]++;
+                maxiC=max(maxiC, c[ch]);
+            }
         }
+        // for(int i=0; i<s.length(); i++){
+        //     mpp[s[i]]++;
+        // }  
+        // for(auto& it: mpp){
+        //     if(it.first=='a' || it.first=='e' || it.first=='i' || it.first=='o' || it.first=='u')
+        //         maxiV=max(maxiV, it.second);
+        //     else   
+        //         maxiC=max(maxiC, it.second);
+        // }
         return maxiV+maxiC;
     }
 };
