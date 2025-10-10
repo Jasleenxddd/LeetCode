0054c4d95e1722fd8b2eb62d88d6401520f0d82f
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2025-10-10 16:36:38 +0530

    Time: 7 ms (51.41%) | Memory: 13.3 MB (37.64%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     int romanToInt(string s) {
-         unordered_map<char, int> romanMap = {
-        {'I', 1},
-        {'V', 5},
-        {'X', 10},
-        {'L', 50},
-        {'C', 100},
-        {'D', 500},
-        {'M', 1000}
-    };
-
-    int total = 0;
-    int prevValue = 0;
-
-    // Iterate from right to left
-    for (int i = s.size() - 1; i >= 0; --i) {
-        int value = romanMap[s[i]];
-
-        if (value < prevValue) {
-            // We subtract if current value is less than previous (e.g., IV)
-            total -= value;
-        } else {
-            total += value;
+        unordered_map<char, int> mpp={
+            {'I', 1},
+            {'V', 5}, 
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        int n=s.size();
+        int prev=0;
+        int ans=0;
+        for(int i=n-1; i>=0; i--){
+            int val=mpp[s[i]];
+            if(val<prev) ans-=val;
+            else ans+=val;
+            prev=val;
         }
-
-        prevValue = value;
+        return ans;
     }
-
-    return total;
-}
 };
