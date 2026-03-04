9c0d717e24fbe570b3188701b6f994b225f2b536
Jasleen Kaur <jasleenkaurvasir@gmail.com>
2026-03-04 23:29:51 +0530

    Time: 1 ms (28.09%) | Memory: 17.2 MB (20.57%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -3,27 +3,46 @@ public:
     int numSpecial(vector<vector<int>>& mat) {
         int m=mat.size();
         int n=mat[0].size();
-        queue<pair<int,int>> q;
+
+        // queue<pair<int,int>> q;
+        // for(int i=0; i<m; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(mat[i][j]==1) q.push({i, j});
+        //     }
+        // }
+        // int ans=0;
+        // while(!q.empty()){
+        //     int r=q.front().first;
+        //     int c=q.front().second;
+        //     q.pop();
+        //     bool special=true;
+
+        //     for(int i=0; i<m; i++){
+        //         if(i!=r && mat[i][c]==1) special=false;
+        //     }
+
+        //     for(int j=0; j<n; j++){
+        //         if(j!=c && mat[r][j]==1) special=false;
+        //     }
+        //     if(special) ans++;
+        // }
+        // return ans;
+
+        vector<int> rows(m, 0);
+        vector<int> cols(n, 0);
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(mat[i][j]==1) q.push({i, j});
+                if(mat[i][j]==1){
+                    rows[i]++;
+                    cols[j]++;
+                }
             }
         }
         int ans=0;
-        while(!q.empty()){
-            int r=q.front().first;
-            int c=q.front().second;
-            q.pop();
-            bool special=true;
-
-            for(int i=0; i<m; i++){
-                if(i!=r && mat[i][c]==1) special=false;
-            }
-
+        for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(j!=c && mat[r][j]==1) special=false;
+                if(mat[i][j]==1 && rows[i]==1 && cols[j]==1) ans++;
             }
-            if(special) ans++;
         }
         return ans;
     }
